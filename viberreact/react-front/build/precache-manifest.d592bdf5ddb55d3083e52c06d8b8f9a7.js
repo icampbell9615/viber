e902a256f7ad8a74fc523b9f9f1014c84bf7c1dd
Campbell,Ian <icampbell9615@gmail.com>
2019-10-01 16:21:43 -0500

    Restored Old Backend

---
+++ viberreact/react-front/build/precache-manifest.d592bdf5ddb55d3083e52c06d8b8f9a7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb4d0eed1d7dfa5a2b63f13e607994a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f9341cae20e50b12a80",
+    "url": "/static/js/2.72d831e5.chunk.js"
+  },
+  {
+    "revision": "1bd7510b335b035593c6",
+    "url": "/static/js/main.0632d1d5.chunk.js"
+  },
+  {
+    "revision": "a0d5f7216f0d6e5d69aa",
+    "url": "/static/js/runtime-main.0d8fa276.js"
+  }
+]);
--- viberreact/react-front/src/App.js
+++ viberreact/react-front/src/App.js
@@ -2,6 +2,23 @@ import React from "react";
 import {BrowserRouter} from 'react-router-dom'
 import MainRouter from './MainRouter'
 
+const mongoose = require ('mongoose');
+// load env variables
+const dotenv = require('dotenv');
+dotenv.config()
+ 
+//db connection
+mongoose.connect(
+  process.env.MONGO_URI,
+  {useNewUrlParser: true}
+)
+.then(() => console.log('DB Connected'))
+ 
+mongoose.connection.on('error', err => {
+  console.log(`DB connection error: ${err.message}`)
+});
+
+
 const App = () => (
 <BrowserRouter>
  <MainRouter />

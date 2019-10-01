315c9063ba7feabe8b8365fc1402abdbbd19528d
Campbell,Ian <icampbell9615@gmail.com>
2019-10-01 11:17:49 -0500

    Master Branch Access

--- viberreact/react-front/src/user/Signup.js
+++ viberreact/react-front/src/user/Signup.js
@@ -1,4 +1,23 @@
 import React, {Component} from "react";
+const express=require("express"); 
+const bodyParser=require("body-parser"); 
+const mongoose = require('mongoose'); 
+
+
+mongoose.connect('mongodb://localhost:27017/signup'); 
+var db=mongoose.connection; 
+db.on('error', console.log.bind(console, "connection error")); 
+db.once('open', function(callback){ 
+    console.log("connection succeeded"); 
+}) 
+
+var app=express() 
+app.use(bodyParser.json()); 
+app.use(express.static('public')); 
+app.use(bodyParser.urlencoded({ 
+    extended: true
+})); 
+
 
 class Signup extends Component {
      constructor() {
